8bfb6e8b649d3c281db288b1c788ecf398033f9d
Adam Gaia <git@agaia.dev>
2025-04-28 11:16:18 -0600

    feat: start adding trycmd tests; code changes to pass test

--- crates/different/src/lib.rs
+++ crates/different/src/lib.rs
@@ -1,27 +1,16 @@
 use colored::{Color, Colorize};
 use std::{fmt::Display, str::FromStr};
 
-fn display_str(num: Option<usize>) -> String {
-    if let Some(num) = num {
-        num.to_string()
-    } else {
-        String::from(" ")
-    }
-}
+const DEFAULT_LEFT_MARKER: char = '-';
+const DEFAULT_RIGHT_MARKER: char = '+';
+const DEFAULT_MARKER_COUNT: usize = 4;
+const DEFAULT_INDENT_SPACES: usize = 2;
+const DEFAULT_LEFT_COLOR: Color = Color::Green;
+const DEFAULT_RIGHT_COLOR: Color = Color::Red;
+use anyhow::Result;
 
-#[derive(Debug)]
-pub enum Diff {
-    Same,
-    Diff(String),
-}
-
-impl Display for Diff {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Same => write!(f, ""),
-            Self::Diff(diff) => write!(f, "{diff}"),
-        }
-    }
+fn parse_color(s: &str) -> Result<Color> {
+    todo!();
 }
 
 #[derive(Debug)]
@@ -43,7 +32,13 @@ fn header(side: Side, name: Option<&String>, marker: char, marker_len: usize) ->
     let marker_bar = marker.to_string().repeat(marker_len);
 
     let name = match name {
-        Some(name) => format!("{side}: {name}"),
+        Some(name) => {
+            let extra_space = match side {
+                Side::Left => " ", // 'right' is one more character than 'left' so we need a padding space
+                Side::Right => "",
+            };
+            format!("{side}:{extra_space} {name}")
+        }
         None => format!("{side}"),
     };
 
@@ -56,103 +51,152 @@ enum ColorSide {
     Both,
 }
 
-/// Compare 'expected' to 'actual', where 'actual' is (well probably) a modified version of 'expected'
-/// Returns true if the inputs are the same, false if different.
-/// If 'print' is true and there are differences, prints the diff
-pub fn line_diff(left: &str, right: &str, settings: &DiffSettings, print: bool) -> bool {
-    let mut same = true;
-    let diff = diff::lines(left, right);
-
-    let left_color = if let Some(color) = settings.left_color {
-        color
-    } else {
-        DEFAULT_LEFT_COLOR
-    };
-
-    let right_color = if let Some(color) = settings.right_color {
-        color
-    } else {
-        DEFAULT_RIGHT_COLOR
-    };
-
-    let indent = " ".repeat(settings.indent_spaces);
-
-    if print {
-        // TODO: force color and no color should be mutually exclusive
-        if settings.force_color {
-            colored::control::set_override(true);
-        }
-        if settings.no_color {
-            colored::control::set_override(false);
-        }
-
-        let left_header = header(
-            Side::Left,
-            settings.left_name.as_ref(),
-            settings.left_marker,
-            settings.marker_count,
-        )
-        .color(left_color);
-        let right_header = header(
-            Side::Right,
-            settings.right_name.as_ref(),
-            settings.right_marker,
-            settings.marker_count,
-        )
-        .color(right_color);
-        println!("{left_header}");
-        println!("{right_header}");
+fn display_str(num: Option<usize>, max_width: Option<usize>) -> String {
+    match max_width {
+        Some(width) => match num {
+            Some(num) => format!("{num:>width$}"),
+            None => " ".repeat(width),
+        },
+        None => match num {
+            Some(num) => num.to_string(),
+            None => String::from(" "),
+        },
     }
-
-    let mut line_num_a = 0;
-    let mut line_num_b = 0;
-    for line in diff {
-        let (sep, content, line_num_a_display, line_num_b_display, color) = match line {
-            diff::Result::Left(l) => {
-                same = false;
-                line_num_a += 1;
-                ('-', l, Some(line_num_a), None, ColorSide::Left)
-            }
-            diff::Result::Both(l, _) => {
-                line_num_a += 1;
-                line_num_b += 1;
-                ('|', l, Some(line_num_a), Some(line_num_b), ColorSide::Both)
-            }
-            diff::Result::Right(r) => {
-                same = false;
-                line_num_b += 1;
-                ('+', r, None, Some(line_num_b), ColorSide::Right)
-            }
-        };
-
-        if print {
-            let line_num_a_display = display_str(line_num_a_display);
-            let line_num_b_display = display_str(line_num_b_display);
-
-            let line =
-                format!("{indent}{line_num_a_display}{indent}{line_num_b_display} {sep} {content}");
-            let line = match color {
-                ColorSide::Left => line.color(left_color),
-                ColorSide::Right => line.color(right_color),
-                ColorSide::Both => line.dimmed(),
-            };
-            println!("{line}");
-        }
+    /*
+    if let Some(num) = num {
+        num.to_string()
+    } else {
+        String::from(" ")
     }
-
-    same
+    */
 }
 
-const DEFAULT_LEFT_MARKER: char = '-';
-const DEFAULT_RIGHT_MARKER: char = '+';
-const DEFAULT_MARKER_COUNT: usize = 4;
-const DEFAULT_INDENT_SPACES: usize = 2;
-const DEFAULT_LEFT_COLOR: Color = Color::Green;
-const DEFAULT_RIGHT_COLOR: Color = Color::Red;
-use anyhow::Result;
+#[derive(Debug)]
+pub enum Diff<'a, T> {
+    Same,
+    Diff {
+        settings: &'a DiffSettings,
+        diff: Vec<diff::Result<T>>,
+    },
+}
 
-fn parse_color(s: &str) -> Result<Color> {
-    todo!();
+impl<'a, T> Display for Diff<'a, T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Same => write!(f, "")?,
+            Self::Diff { settings, diff } => {
+                let max_num_width = settings.max_line_number.map(|x| x.ilog10() as usize + 1);
+
+                let left_color = if let Some(color) = settings.left_color {
+                    color
+                } else {
+                    DEFAULT_LEFT_COLOR
+                };
+
+                let right_color = if let Some(color) = settings.right_color {
+                    color
+                } else {
+                    DEFAULT_RIGHT_COLOR
+                };
+
+                let indent = " ".repeat(settings.indent_spaces);
+
+                // TODO: force color and no color should be mutually exclusive
+                if settings.force_color {
+                    colored::control::set_override(true);
+                }
+                if settings.no_color {
+                    colored::control::set_override(false);
+                }
+
+                let left_header = header(
+                    Side::Left,
+                    settings.left_name.as_ref(),
+                    settings.left_marker,
+                    settings.marker_count,
+                )
+                .color(left_color);
+                let right_header = header(
+                    Side::Right,
+                    settings.right_name.as_ref(),
+                    settings.right_marker,
+                    settings.marker_count,
+                )
+                .color(right_color);
+                writeln!(f, "{left_header}")?;
+                writeln!(f, "{right_header}")?;
+
+                let mut line_num_a = 0;
+                let mut line_num_b = 0;
+                for line in diff {
+                    let (sep, content, line_num_a_display, line_num_b_display, color) = match line {
+                        diff::Result::Left(l) => {
+                            line_num_a += 1;
+                            ('-', l, Some(line_num_a), None, ColorSide::Left)
+                        }
+                        diff::Result::Both(l, _) => {
+                            line_num_a += 1;
+                            line_num_b += 1;
+                            ('|', l, Some(line_num_a), Some(line_num_b), ColorSide::Both)
+                        }
+                        diff::Result::Right(r) => {
+                            line_num_b += 1;
+                            ('+', r, None, Some(line_num_b), ColorSide::Right)
+                        }
+                    };
+
+                    let line_num_a_display = display_str(line_num_a_display, max_num_width);
+                    let line_num_b_display = display_str(line_num_b_display, max_num_width);
+
+                    let line = format!(
+                        "{indent}{line_num_a_display}{indent}{line_num_b_display} {sep} {content}"
+                    );
+                    let line = match color {
+                        ColorSide::Left => line.color(left_color),
+                        ColorSide::Right => line.color(right_color),
+                        ColorSide::Both => line.dimmed(),
+                    };
+                    writeln!(f, "{line}")?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+/// Compare 'expected' to 'actual', where 'actual' is (well probably) a modified version of 'expected'
+pub fn line_diff<'a>(
+    left: &'a str,
+    right: &'a str,
+    settings: &'a DiffSettings,
+) -> Diff<'a, &'a str> {
+    let mut same = true;
+    let diff = diff::lines(left, right);
+    let mut same = true;
+
+    for line in &diff {
+        match line {
+            diff::Result::Left(_) => {
+                same = false;
+                break;
+            }
+            diff::Result::Both(_, _) => {
+                continue;
+            }
+            diff::Result::Right(_) => {
+                same = false;
+                break;
+            }
+        }
+    }
+    if same {
+        Diff::Same
+    } else {
+        Diff::Diff { settings, diff }
+    }
 }
 
 // TODO: settings for no line numbers, plain style, no header, etc
@@ -189,6 +233,9 @@ pub struct DiffSettings {
 
     #[clap(long)]
     no_color: bool,
+
+    #[clap(skip)]
+    max_line_number: Option<usize>,
 }
 
 impl DiffSettings {
@@ -200,6 +247,11 @@ impl DiffSettings {
     pub fn names(mut self, left: String, right: String) -> Self {
         self.left_name = Some(left);
         self.right_name = Some(right);
+        self
+    }
+
+    pub fn max_line_number(mut self, n: usize) -> Self {
+        self.max_line_number = Some(n);
         self
     }
 }
@@ -217,6 +269,7 @@ impl Default for DiffSettings {
             left_color: Some(DEFAULT_LEFT_COLOR),
             right_color: Some(DEFAULT_RIGHT_COLOR),
             no_color: false,
+            max_line_number: None,
         }
     }
 }
--- crates/different/src/main.rs
+++ crates/different/src/main.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
 use clap::Parser;
-use different::{DiffSettings, line_diff};
+use different::{line_diff, DiffSettings};
 use log::debug;
-use std::fs;
+use pathdiff::diff_paths;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
 use std::path::PathBuf;
+use std::{env, fs};
 
 #[derive(Parser)]
 struct Cli {
@@ -17,13 +21,41 @@ struct Cli {
     settings: DiffSettings,
 }
 
+fn display_name(path: &Path, cwd: &Path) -> String {
+    diff_paths(&path, &cwd)
+        .map(|p| format!("./{}", p.display()))
+        .unwrap_or(path.display().to_string())
+}
+
+/// Returns (Name: String, contents: String, num_lines: usize)
+fn process_file(path: &Path, cwd: &Path) -> Result<(String, String, usize)> {
+    let path = path.canonicalize()?;
+    let name = display_name(&path, cwd);
+    let contents = fs::read_to_string(path)?;
+    let num_lines = contents.lines().count();
+    Ok((name, contents, num_lines))
+}
+
 fn main() -> Result<()> {
     env_logger::init();
+    let cwd = env::current_dir()?;
     let args = Cli::parse();
-    let left = fs::read_to_string(args.left)?;
-    let right = fs::read_to_string(args.right)?;
-    let settings = args.settings;
+
+    let left = PathBuf::from(args.left);
+    let right = PathBuf::from(args.right);
+
+    let (left_name, left_contents, left_num_lines) = process_file(&left, &cwd)?;
+    let (right_name, right_contents, right_num_lines) = process_file(&right, &cwd)?;
+
+    let num_lines = std::cmp::max(left_num_lines, right_num_lines);
+    let settings = args
+        .settings
+        .names(left_name, right_name)
+        .max_line_number(num_lines);
     debug!("{settings:?}");
-    line_diff(&left, &right, &settings, true);
+
+    let diff = line_diff(&left_contents, &right_contents, &settings);
+    println!("{diff}");
+
     Ok(())
 }
