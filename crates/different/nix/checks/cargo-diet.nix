3daa98821aafff80a1a9054d5638f1ef9282d044
Adam Gaia <git@agaia.dev>
2025-04-29 17:43:27 -0600

    feat: wrap cargo-diet for exit code checking

--- crates/different/nix/checks/cargo-diet.nix
+++ crates/different/nix/checks/cargo-diet.nix
@@ -8,15 +8,24 @@
   craneLib = crateBuilder.craneLib;
   cargoArtifacts = crateBuilder.cargoArtifacts;
 
+  # Cargo diet by default exits with 0 when changes are found. This wrapper script changes that
+  cargo-diet-wrapped = pkgs.writeShellApplication {
+    name = "diet";
+    runtimeInputs = [pkgs.cargo-diet];
+    text = ''
+      [[ "$(cargo diet "$@" | tail -n 1)" == "There would be no change." ]] || exit 1
+    '';
+  };
+
   # Run cargo diet
   cargo-diet = craneLib.mkCargoDerivation (commonArgs
     // {
-      buildPhaseCargoCommand = "cargo diet --dry-run";
+      buildPhaseCargoCommand = "${cargo-diet-wrapped}/bin/diet --dry-run";
 
       inherit cargoArtifacts;
 
       pnameSuffix = "-diet";
-      nativeBuildInputs = (commonArgs.nativeBuildInputs or []) ++ [pkgs.cargo-diet];
+      nativeBuildInputs = commonArgs.nativeBuildInputs or [];
     });
 in
   cargo-diet
